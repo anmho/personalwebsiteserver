0ec12461fe74ee1259da4772d3670a180b4c0fd4
Andrew Ho <andyminhtuanho@gmail.com>
2022-07-06 22:44:49 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import requests
+
+from flask import Flask, send_file
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app, origins=["*"])
+
+
+@app.route("/", methods=["GET"])
+def get_pdf():
+    url = "https://github.com/anmho/resume/raw/main/main.pdf"
+    response = requests.get(url, stream=True)
+
+    print(response.status_code)
+
+    with open("resume.pdf", "wb") as resume:
+        resume.write(response.content)
+
+    return send_file("resume.pdf")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
